2e65849c2167ae27ea009eb5bee469cfebbb6b8b
antairon <air@kbp1-lmc-17826.synapse.com>
2014-09-02 20:04:22 +0300

    Initial Commit

---
+++ Training/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Training
+//
+//  Created by Anatolii Reva on 9/2/14.
+//  Copyright (c) 2014 AIR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+

16878887a65c2b2ac1d0b52e7a4a10ba45e8384d
antairon <air@kbp1-lmc-17826.local>
2014-09-05 21:56:55 +0300

    release )))

--- Training/main.m
+++ Training/main.m
@@ -1,22 +1,113 @@
-//
-//  main.m
-//  Training
-//
-//  Created by Anatolii Reva on 9/2/14.
-//  Copyright (c) 2014 AIR. All rights reserved.
-//
+// Implement a Calculator class
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[])
+@interface Calculator: NSObject
 {
-
-    @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
-    }
-    return 0;
+    double accumulator, memory;
 }
 
+// accumulator methods
+-(void) setAccumulator: (double) value;
+-(void) clear;
+-(double) accumulator;
+
+//Memory methods
+
+-(void) memoryClear;
+-(void) memoryStore;
+-(double) memoryRecall;
+-(double) memoryAdd: (double) value;
+-(double) memorySubstract: (double) value;
+
+// arithmetic methods
+-(double) add: (double) value;
+-(double) subtract: (double) value;
+-(double) multiply: (double) value;
+-(double) divide: (double) value;
+@end
+
+@implementation Calculator
+-(void) memoryClear
+{
+    memory = 0;
+}
+-(double) memoryRecall
+{
+    return memory;
+}
+-(void) memoryStore
+{
+    memory = accumulator;
+}
+-(double) memoryAdd:(double) value
+{
+    memory += value;
+    return memory;
+}
+-(double) memorySubstract:(double)value
+{
+    memory-= value;
+    return memory;
+}
+-(void) setAccumulator: (double) value
+{
+    accumulator = value;
+}
+
+-(void) clear;
+{
+    accumulator = 0;
+}
+
+-(double) accumulator
+{
+    return accumulator;
+}
+
+-(double) add: (double) value
+{
+    accumulator += value;
+    return accumulator;
+}
+
+-(double) subtract: (double) value
+{
+    accumulator -= value;
+        return accumulator;
+}
+
+-(double) multiply: (double) value
+{
+    accumulator *= value;
+        return accumulator;
+}
+
+-(double) divide: (double) value;
+{
+    accumulator /= value;
+    return accumulator;
+}
+@end;
+
+int main (int argc, char *argv[])
+{
+    @autoreleasepool {
+    
+    Calculator *deskCalc = [[Calculator alloc] init];
+    
+    [deskCalc clear];
+        NSLog(@"Initial number is 100");
+        [deskCalc setAccumulator:100.];
+        NSLog(@"Adding 30 - result %f", [deskCalc add:30]);
+        NSLog(@"Substract 10 - result %f", [deskCalc subtract:10]);
+        NSLog(@"Multiply by 3 - result %f", [deskCalc multiply:3]);
+        NSLog(@"Divide by 2 - result %f", [deskCalc divide:2]);
+        NSLog(@"Put result in memory");
+        [deskCalc memoryClear];
+        [deskCalc memoryStore];
+        NSLog(@"Add 10 to memory: result %f", [deskCalc memoryAdd:10]);
+        NSLog(@"Substract 5: %f", [deskCalc memorySubstract:5]);
+        }
+    return 0;
+}

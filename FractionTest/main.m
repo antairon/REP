b930fdff03edd34ecd36a757741bd95702de3fd4
Anatolii Reva <air@Anatoliis-MacBook-Pro-10.local>
2014-09-21 20:55:11 +0300

    Initial Commit

---
+++ FractionTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FractionTest
+//
+//  Created by Anatolii Reva on 9/21/14.
+//  Copyright (c) 2014 Anatolii Reva. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

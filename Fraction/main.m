e366cab97d61736fd3495fe78dbb8391a519d633
antairon <air@kbp1-lmc-17826.local>
2014-10-02 00:24:07 +0300

    Initial Commit

---
+++ Fraction/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Fraction
+//
+//  Created by Anatolii Reva on 10/2/14.
+//  Copyright (c) 2014 AIR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
